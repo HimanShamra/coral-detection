a483da2ee2762b9017f39d45228d020057a03d88
Himanshu Sharma <himanshamra@gmail.com>
2019-01-04 14:28:05 -0500

    Clean up

--- coraldetect.py
+++ coraldetect.py
@@ -1,12 +1,12 @@
 import cv2
 import numpy as np
 
-face_cascade = cv2.CascadeClassifier('myhaarc.xml')
+cascade = cv2.CascadeClassifier('myhaarc.xml')
 
 img = cv2.imread('corals.jpg');
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-faces = face_cascade.detectMultiScale(gray,1.3,5)
-for (x,y,w,h) in faces:
+coral = cascade.detectMultiScale(gray,1.3,5)
+for (x,y,w,h) in coral:
     cv2.rectangle(img, (x,y),(x+w,y+h),(0,255,0),3)  
 cv2.imwrite('563.jpg',img)
 cv2.waitKey(0)
